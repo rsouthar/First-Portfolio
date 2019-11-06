32af89d2b6a25289d7b706474d4144831e570382
Rick <rdsouthard1979@gmail.com>
2019-11-06 06:35:41 -0600

    Added JS file

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.sr =  ScrollReveal({reset: false});
+sr.reveal('.homeText', { duration: 1000,
+                         origin: 'bottom',
+                         distance: '45px'}, 70);
+sr.reveal('.nextText', { duration: 500,
+                         origin: 'bottom',
+                         distance: '45px',
+                         viewFactor: 0.6}, 40);
+sr.reveal('.skillText', {duration: 500,
+                        origin: 'bottom',
+                        distance: '45px',
+                        viewFactor: 0.7}, 40);
+sr.reveal('.myWork', {duration: 500,
+                      origin: 'right',
+                      viewFactor: 0.5}, 100);
+sr.reveal('.myProjects', {duration: 500,
+                         origin: 'right',
+                         viewFactor: 0.5}, 100);
+sr.reveal('.myWeb', {duration: 500,
+                    origin: 'right',
+                    viewFactor: 0.5}, 100);
